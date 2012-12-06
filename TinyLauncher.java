5936ec07a5fdad00a528193161a4c1bc697ba83d
Kyle Klaus <kklaus@indemnity83.com>
2012-12-05 22:21:52 -0800

    fleshed out getUserSession

--- TinyLauncher.java
+++ TinyLauncher.java
@@ -1,17 +1,11 @@
-import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipException;
-import java.util.zip.ZipFile;
+import javax.swing.*;
+import java.awt.*;
 
 public class TinyLauncher
 {
-	protected String[] session = new String[3];
+	protected static String[] session = new String[3];
 
-    public static void main(String[] args)
+    public static void main()
 	{
 	    getUserSession();
 		getUpdates();
@@ -21,25 +15,25 @@ public class TinyLauncher
 		// Lets do it!
 		session[2] = "Oakhart";      // Window Name
 		session[3] = "max";          // Start maximized
-		MultiMCLauncher launcher = new MultiMCLauncher(session);
-				
+		MultiMCLauncher launcher = new MultiMCLauncher();
+		launcher.main(session);				
 	}
 	
-	public void getUpdates() {
+	public static void getUpdates() {
 		// TODO: Figure this out! Do we check against our specific server? Probably better to 
 		// check against the jenkins system for the latest tagged version. 
 		//
 		// Check out http://main.brothersklaus.com:8080/job/Oakheart/lastSuccessfulBuild/api/json?pretty=true
 	}
 	
-	public void getJar()
+	public static void getJar()
 	{
 	    // Get latest jar!
 		// https://s3.amazonaws.com/MinecraftDownload/minecraft.jar
 		// TODO: Is there a way to insure we have the right version?
 	}
 	
-	public void installJarMods()
+	public static void installJarMods()
 	{
 	    // check if we already have a backup jar, if we do just return
 		// Create a backup of the main jar
@@ -49,16 +43,16 @@ public class TinyLauncher
 		// Clean up any temp folders/files
 	}
 	
-	public String[] getUserSession();
+	public static void getUserSession()
 	{
 	    // Prompt user for credentials somehow
 		// get session via https://login.minecraft.net/?user=%USER%&password=%PASS%&version=14
 		// Check that return result is valid
 		// Return the username and session string
 		// TODO: Make sure we don't expose the password somehow, IE force garbage collection?
-		
 		session[0] = "Indemnity83";
-		session[1] = "Pass";
+		session[1] = "Dell700m";
+		
 	}
 	
 }
